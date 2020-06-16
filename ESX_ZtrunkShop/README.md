2dd9450b16965ad66f5a66694ef79f39cdfbc4f8
ztrunk <ztrunk@protonmail.com>
2020-06-16 18:53:38 +0200

    Add files via upload

---
+++ ESX_ZtrunkShop/README.md
@@ -0,0 +1,24 @@
+# Fivem ESX_ZtrunkShop RO
+
+[ Info ]
+
+- Scriptul este bazat ESX , dependete pentru utilizare completa > 
+
+
+- 100 % modificabil usor de optimizat !
+
+
+[ Instalatie ]
+
+1. Descarcal
+2. Punel in Resources
+3. Ai nevoie sa ai produsele in baza de date SQL e doar un exemplu poti folosii orice produs din baza ta de date produsele de adauga in "config.lua" 
+4. Dai start in server.lua 
+
+
+[ Necesare ]
+
+- ESX
+- esx_menu_default
+- pNotify
+
---
+++ ESX_ZtrunkShop/SQL.sql
@@ -0,0 +1,19 @@
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
+	('cocacola', 'Coca Cola', -1, 0, 1),
+	('fanta', 'Fanta ', -1, 0, 1),
+	('sprite', 'Sprite', -1, 0, 1),
+	('loka', 'Loka', -1, 0, 1),
+	
+	('cheesebows', 'Pufuleti', -1, 0, 1),
+	('chips', 'Chipsuri', -1, 0, 1),
+	('marabou', 'Marabou ', -1, 0, 1),
+	
+	('pizza', 'Kebab', -1, 0, 1),
+	('burger', 'Hamburgher', -1, 0, 1),
+	('pastacarbonara', 'Carbonara', -1, 0, 1),
+	('macka', 'Tocanita', -1, 0, 1),
+	
+	('cigarett', 'Tigari', -1, 0, 1),
+	('lighter', 'Lanterna', -1, 0, 1),
+	('lotteryticket', 'Bilet la loterie', -1, 0, 1)
+;
